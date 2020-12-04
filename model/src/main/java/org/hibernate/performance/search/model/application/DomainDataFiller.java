d0d731b0a93f8bf52e57680e3673ed5146baf4b6
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2020-12-04 12:30:31 +0100

    HSEARCH-3107 Extract the DomainDataFiller
    So that it can be reused by the other modules

---
+++ model/src/main/java/org/hibernate/performance/search/model/application/DomainDataFiller.java
@@ -0,0 +1,78 @@
+package org.hibernate.performance.search.model.application;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.performance.search.model.entity.Company;
+import org.hibernate.performance.search.model.entity.Employee;
+import org.hibernate.performance.search.model.entity.Manager;
+import org.hibernate.performance.search.model.entity.answer.QuestionnaireInstance;
+import org.hibernate.performance.search.model.entity.question.QuestionnaireDefinition;
+import org.hibernate.performance.search.model.service.AnswerFiller;
+import org.hibernate.performance.search.model.service.CompanyFactory;
+import org.hibernate.performance.search.model.service.EmployeeFactory;
+import org.hibernate.performance.search.model.service.EmployeeRepository;
+import org.hibernate.performance.search.model.service.QuestionnaireDefinitionFactory;
+import org.hibernate.performance.search.model.service.QuestionnaireInstanceFactory;
+import org.hibernate.performance.search.model.service.Scorer;
+
+public class DomainDataFiller {
+
+	private final SessionFactory sessionFactory;
+
+	public DomainDataFiller(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public void fillData(int companyId) {
+		// atomicity is not used here
+		AtomicReference<Company> companyReference = new AtomicReference<>();
+
+		// Phase 1: create the company and its business units
+		HibernateORMHelper.inTransaction( sessionFactory, session -> {
+			Company company = CompanyFactory.createCompanyAndUnits( companyId );
+			session.persist( company );
+			companyReference.set( company );
+		} );
+
+		// Phase 2: create the employees' organization chart
+		HibernateORMHelper.inTransaction( sessionFactory, session -> {
+			Manager ceo = EmployeeFactory.createEmployeeTree( companyReference.get() );
+			session.persist( ceo );
+		} );
+
+		// Phase 3: define the questionnaires
+		List<QuestionnaireDefinition> questionnaireDefinitions = QuestionnaireDefinitionFactory
+				.createQuestionnaireDefinitions( companyReference.get() );
+		for ( QuestionnaireDefinition questionnaire : questionnaireDefinitions ) {
+			HibernateORMHelper.inTransaction( sessionFactory, session -> session.persist( questionnaire ) );
+		}
+
+		// Phase 4: instantiate questionnaires for the employees
+		HibernateORMHelper.inTransaction( sessionFactory, session -> {
+			EmployeeRepository repository = new EmployeeRepository( session );
+			List<Employee> employees = repository.getEmployees( companyReference.get() );
+			List<QuestionnaireDefinition> definitions = repository.getQuestionnaireDefinitions(
+					companyReference.get() );
+
+			for ( Employee employee : employees ) {
+				for ( QuestionnaireDefinition definition : definitions ) {
+					List<QuestionnaireInstance> questionnaireInstances = QuestionnaireInstanceFactory
+							.createQuestionnaireInstances( employee, definition );
+
+					for ( QuestionnaireInstance instance : questionnaireInstances ) {
+						session.persist( instance );
+					}
+				}
+			}
+		} );
+
+		// Phase 5: simulate the employees filling the questionnaires
+		new AnswerFiller( sessionFactory ).fillAllAnswers();
+
+		// Phase 6: evaluate the performances based on the outputs of the questionnaires
+		new Scorer( sessionFactory ).generateScoreForQuestionnaires( companyReference.get() );
+	}
+
+}
--- model/src/test/java/org/hibernate/performance/search/model/DomainDataFillerIT.java
+++ model/src/test/java/org/hibernate/performance/search/model/DomainDataFillerIT.java
@@ -1,19 +1,17 @@
-package org.hibernate.performance.search;
+package org.hibernate.performance.search.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.atomic.AtomicReference;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.performance.search.model.application.DomainDataFiller;
 import org.hibernate.performance.search.model.application.HibernateORMHelper;
 import org.hibernate.performance.search.model.entity.BusinessUnit;
 import org.hibernate.performance.search.model.entity.Company;
 import org.hibernate.performance.search.model.entity.Employee;
 import org.hibernate.performance.search.model.entity.Manager;
-import org.hibernate.performance.search.model.entity.answer.Answer;
 import org.hibernate.performance.search.model.entity.answer.ClosedAnswer;
 import org.hibernate.performance.search.model.entity.answer.OpenAnswer;
 import org.hibernate.performance.search.model.entity.answer.QuestionnaireInstance;
@@ -21,58 +19,16 @@ import org.hibernate.performance.search.model.entity.question.ClosedQuestion;
 import org.hibernate.performance.search.model.entity.question.OpenQuestion;
 import org.hibernate.performance.search.model.entity.question.Question;
 import org.hibernate.performance.search.model.entity.question.QuestionnaireDefinition;
-import org.hibernate.performance.search.model.service.AnswerFiller;
-import org.hibernate.performance.search.model.service.CompanyFactory;
-import org.hibernate.performance.search.model.service.EmployeeFactory;
 import org.hibernate.performance.search.model.service.EmployeeRepository;
-import org.hibernate.performance.search.model.service.QuestionnaireDefinitionFactory;
-import org.hibernate.performance.search.model.service.QuestionnaireInstanceFactory;
-import org.hibernate.performance.search.model.service.Scorer;
 
 import org.junit.jupiter.api.Test;
 
-public class ModelIT {
+public class DomainDataFillerIT {
 
 	@Test
 	public void test() {
 		try ( SessionFactory sessionFactory = HibernateORMHelper.buildSessionFactory( new Properties() ) ) {
-			// atomicity is not used here
-			AtomicReference<Company> companyReference = new AtomicReference<>();
-
-			HibernateORMHelper.inTransaction( sessionFactory, session -> {
-				Company company = CompanyFactory.createCompanyAndUnits( 0 );
-				session.persist( company );
-				companyReference.set( company );
-			} );
-
-			HibernateORMHelper.inTransaction( sessionFactory, session -> {
-				Manager ceo = EmployeeFactory.createEmployeeTree( companyReference.get() );
-				session.persist( ceo );
-			} );
-
-			List<QuestionnaireDefinition> questionnaireDefinitions = QuestionnaireDefinitionFactory
-					.createQuestionnaireDefinitions( companyReference.get() );
-			for ( QuestionnaireDefinition questionnaire : questionnaireDefinitions ) {
-				HibernateORMHelper.inTransaction( sessionFactory, session -> session.persist( questionnaire ) );
-			}
-
-			HibernateORMHelper.inTransaction( sessionFactory, session -> {
-				EmployeeRepository repository = new EmployeeRepository( session );
-				List<Employee> employees = repository.getEmployees( companyReference.get() );
-				List<QuestionnaireDefinition> definitions = repository.getQuestionnaireDefinitions(
-						companyReference.get() );
-
-				for ( Employee employee : employees ) {
-					for ( QuestionnaireDefinition definition : definitions ) {
-						List<QuestionnaireInstance> questionnaireInstances = QuestionnaireInstanceFactory
-								.createQuestionnaireInstances( employee, definition );
-
-						for (QuestionnaireInstance instance : questionnaireInstances) {
-							session.persist( instance );
-						}
-					}
-				}
-			} );
+			new DomainDataFiller( sessionFactory ).fillData( 0 );
 
 			try ( Session session = sessionFactory.openSession() ) {
 				EmployeeRepository repository = new EmployeeRepository( session );
@@ -92,16 +48,12 @@ public class ModelIT {
 				assertThat( repository.count( ClosedAnswer.class ) ).isEqualTo( 118800 );
 			}
 
-			new AnswerFiller( sessionFactory ).fillAllAnswers();
-
 			try ( Session session = sessionFactory.openSession() ) {
 				EmployeeRepository repository = new EmployeeRepository( session );
 
 				assertThat( repository.countFilledOpenAnswer() ).isEqualTo( 118800 );
 				assertThat( repository.countFilledClosedAnswer() ).isEqualTo( 118800 );
 			}
-
-			new Scorer( sessionFactory ).generateScoreForQuestionnaires( companyReference.get() );
 		}
 	}
 }
