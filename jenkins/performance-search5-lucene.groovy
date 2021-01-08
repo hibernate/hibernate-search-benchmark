8160758dc81857850d1ceb6298b8fdb00bc26f24
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-01-08 17:35:22 +0100

    HSEARCH-3107 Add basic performance pipeline: lucene+search5

---
+++ jenkins/performance-search5-lucene.groovy
@@ -0,0 +1,50 @@
+pipeline {
+    agent any
+    tools {
+        maven "Apache Maven 3.6"
+        jdk "OpenJDK 11 Latest"
+    }
+    stages {
+        stage('Initialize') {
+            steps {
+                sh '''
+                    echo "PATH = ${PATH}"
+                    echo "M2_HOME = ${M2_HOME}"
+                '''
+            }
+        }
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Build') {
+            steps {
+                sh """ \
+					mvn clean install -P search5 \
+					-U -pl jmh-lucene -am \
+					-DskipTests -Ddocker.skip \
+			"""
+                dir('jmh-lucene/target') {
+                    stash name: 'jar', includes: 'benchmarks.jar'
+                }
+            }
+        }
+        stage('Performance test') {
+            steps {
+                unstash name: 'jar'
+                sh 'docker run --name postgresql -p 5431:5432 -e POSTGRES_USER=username -e POSTGRES_PASSWORD=password -e POSTGRES_DB=database -d postgres:10.5'
+                sh 'mkdir -p output'
+                sh """ \
+					java \
+					-jar benchmarks.jar \
+					-wi 1 -i 10 \
+					-rff output/benchmark-results-search5-lucene.csv \
+			"""
+                sh 'docker stop postgresql'
+                sh 'docker rm -f postgresql'
+                archiveArtifacts artifacts: 'output/**'
+            }
+        }
+    }
+}
