53ffb876bd82457e5711d235ebd450ab0a7b91cc
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2021-02-03 10:25:21 +0100

    Show container list status before and after prune

--- jenkins/docker-prune.sh
+++ jenkins/docker-prune.sh
@@ -3,7 +3,13 @@
 
 set -e
 
+echo 'Container list before prune:'
+docker ps -as
+
 docker container prune -f
 docker image prune -f
 docker network prune -f
 docker volume prune -f
+
+echo 'Container list after prune:'
+docker ps -as
