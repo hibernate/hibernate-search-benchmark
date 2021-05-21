fba3079727a04c47962f64ed84d0d7b9d0b71592
Yoann Rodière <yoann@hibernate.org>
2021-05-21 14:30:58 +0200

    Revert RelationshipSize changes
    I shouldn't have done that in the same commit that changed the default
    relationship size.

--- model/src/main/java/org/hibernate/search/benchmark/model/param/RelationshipSize.java
+++ model/src/main/java/org/hibernate/search/benchmark/model/param/RelationshipSize.java
@@ -3,7 +3,7 @@ package org.hibernate.search.benchmark.model.param;
 public enum RelationshipSize {
 
 	SMALL( 1, 1, 1, new int[] { 3 }, 1 ),
-	MEDIUM( 2, 5, 2, new int[] { 7, 3, 9 }, 2 ),
+	MEDIUM( 2, 3, 2, new int[] { 7, 3 }, 2 ),
 	LARGE( 10, 10, 10, new int[] { 7, 3, 9, 3, 3, 9, 7, 7, 7, 9 }, 10 );
 
 	private final int unitsPerCompany;
